52d846600a843b4b5f10a35bc8b38ffd1041283d
wybosys <wybosys@gmail.com>
2018-06-07 11:35:25 +0800

    sample

--- app/sample.php
+++ app/sample.php
@@ -2,7 +2,51 @@
 
 namespace App;
 
+use Nnt\Core\DateTime;
+use Nnt\Core\IRouter;
 use Nnt\Server\Rest;
+use Nnt\Server\Transaction;
+
+/**
+ * @model()
+ */
+class Echoo
+{
+    /**
+     * @string(1, [input], "输入")
+     */
+    public $input;
+
+    /**
+     * @string(2, [output], "输出")
+     */
+    public $output;
+
+    /**
+     * @integer(3, [output], "服务器时间")
+     */
+    public $time;
+}
+
+class RSample implements IRouter
+{
+
+    function action()
+    {
+        return "sample";
+    }
+
+    /**
+     * @action(Echoo)
+     */
+    function echo(Transaction $trans)
+    {
+        $m = $trans->model;
+        $m->output = $m->input;
+        $m->time = DateTime::Current();
+        $trans->submit();
+    }
+}
 
 class Sample extends Rest
 {
@@ -10,5 +54,6 @@ class Sample extends Rest
     function __construct()
     {
         parent::__construct();
+        $this->routers()->register(new RSample());
     }
 }
