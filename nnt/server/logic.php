080c004054fb87f3936cf0830672adff9b9d003f
wybosys <wybosys@gmail.com>
2018-11-08 10:49:25 +0800

    prj

--- nnt/server/logic.php
+++ nnt/server/logic.php
@@ -15,7 +15,7 @@ class Logic extends Server
             return false;
         if (!isset($cfg->host))
             return false;
-        $this->host = $cfg->host;
+        $this->host = self::MapHost($cfg->host);
         return true;
     }
 
@@ -27,5 +27,20 @@ class Logic extends Server
     function stop()
     {
 
+    }
+
+    // php使用域名访问时有可能过慢，所以需要转换仅通过host来访问的地址
+    // 不能转换通过www.xxx.com这类的地址，避免服务器无法通过server_name重定位服务
+    static function MapHost(string $host): string
+    {
+        $url = parse_url($host);
+        if (strpos($url['host'], '.') !== false)
+            return $host;
+        $ip = gethostbyname($url['host']);
+        if (!isset($url['path']))
+            $url['path'] = '';
+        $new = $url['scheme'] . '://' . $ip . $url['path'];
+        echo "logic的host从" . $host . "自动转换为" . $new;
+        return $new;
     }
 }
