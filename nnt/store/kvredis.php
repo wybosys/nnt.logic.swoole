b79db439400fa34896314821fa821aeab2b05f8e
wybosys <wybosys@gmail.com>
2018-11-09 15:27:31 +0800

    支持集群

--- nnt/store/kvredis.php
+++ nnt/store/kvredis.php
@@ -17,7 +17,8 @@ class KvRedis extends Kv
     public $passwd;
     public $timeout = 1;
     public $retry = 0.1;
-    public $prefix = "";
+    public $cluster = false;
+    public $prefix;
 
     function config($cfg): bool
     {
@@ -31,6 +32,8 @@ class KvRedis extends Kv
             $this->dbid = isset($cfg->dbid) ? $cfg->dbid : 0;
         if (isset($cfg->prefix))
             $this->prefix = $cfg->prefix;
+        if (isset($cfg->cluster) && $cfg->cluster)
+            $this->cluster = true;
         $arr = explode(':', $cfg->host);
         $this->host = $arr[0];
         $this->port = count($arr) == 2 ? (int)$arr[1] : self::DEFAULT_PORT;
@@ -52,6 +55,7 @@ class KvRedis extends Kv
         $ret->timeout = $this->timeout;
         $ret->retry = $this->retry;
         $ret->prefix = $this->prefix;
+        $ret->cluster = $this->cluster;
         return $ret;
     }
 
@@ -62,11 +66,21 @@ class KvRedis extends Kv
             return;
         }
 
-        $hdl = new \Redis();
-        $res = $hdl->connect($this->host, $this->port, $this->timeout, null, $this->retry * 1000);
-        if (!$res) {
-            throw new \Exception($hdl->getLastError(), STATUS::EXCEPTION);
-            return;
+        if ($this->cluster) {
+            $hdl = new \RedisCluster(NULL, [
+                $this->host . ':' . $this->port
+            ]);
+            if ($this->prefix)
+                $hdl->setOption(\RedisCluster::OPT_PREFIX, $this->prefix);
+        } else {
+            $hdl = new \Redis();
+            $res = $hdl->connect($this->host, $this->port, $this->timeout, null, $this->retry * 1000);
+            if (!$res) {
+                throw new \Exception($hdl->getLastError(), STATUS::EXCEPTION);
+                return;
+            }
+            if ($this->prefix)
+                $hdl->setOption(\Redis::OPT_PREFIX, $this->prefix);
         }
 
         $this->_hdl = $hdl;
@@ -107,14 +121,8 @@ class KvRedis extends Kv
         $POOLS->push($this->id, $this);
     }
 
-    protected function key(string $key): string
-    {
-        return $this->prefix . $key;
-    }
-
     function get(string $key)
     {
-        $key = $this->key($key);
         $v = $this->_hdl->get($key);
         if ($v === false)
             return null;
@@ -123,7 +131,6 @@ class KvRedis extends Kv
 
     function getraw(string $key)
     {
-        $key = $this->key($key);
         $v = $this->_hdl->get($key);
         if ($v === false)
             return null;
@@ -132,7 +139,6 @@ class KvRedis extends Kv
 
     function setTtl(string $key, int $ttl)
     {
-        $key = $this->key($key);
         if ($ttl == -1) {
             return $this->_hdl->persist($key);
         }
@@ -141,7 +147,6 @@ class KvRedis extends Kv
 
     function ttl(string $key, bool $inseconds = true)
     {
-        $key = $this->key($key);
         if ($inseconds)
             return $this->_hdl->ttl($key);
         return $this->_hdl->pttl($key);
@@ -149,20 +154,17 @@ class KvRedis extends Kv
 
     function set(string $key, Variant $val)
     {
-        $key = $this->key($key);
         $jsstr = $val->serialize();
         return $this->_hdl->set($key, $jsstr);
     }
 
     function setraw(string $key, string $val)
     {
-        $key = $this->key($key);
         return $this->_hdl->set($key, $val);
     }
 
     function getset(string $key, Variant $val)
     {
-        $key = $this->key($key);
         $jsstr = $val->serialize();
         $v = $this->_hdl->getSet($key, $jsstr);
         if ($v === false)
@@ -172,7 +174,6 @@ class KvRedis extends Kv
 
     function getsetraw(string $key, string $val)
     {
-        $key = $this->key($key);
         $v = $this->_hdl->getSet($key, $val);
         if ($v === false)
             return null;
@@ -181,7 +182,6 @@ class KvRedis extends Kv
 
     function del(string $key): DbExecuteStat
     {
-        $key = $this->key($key);
         $ret = $this->_hdl->del($key);
         $r = new DbExecuteStat();
         $r->remove = $ret;
@@ -191,7 +191,6 @@ class KvRedis extends Kv
     // kv数据库通常没有自增函数，所以需要各个业务类自己实现
     function autoinc(string $key, $delta)
     {
-        $key = $this->key($key);
         if ($delta == 1) {
             $ret = $this->_hdl->incr($key);
         } else {
@@ -203,7 +202,6 @@ class KvRedis extends Kv
     // 增加
     function inc(string $key, $delta)
     {
-        $key = $this->key($key);
         if ($delta > 0) {
             if ($delta == 1) {
                 $ret = $this->_hdl->incr($key);
@@ -222,129 +220,107 @@ class KvRedis extends Kv
 
     function lpush(string $key, $val)
     {
-        $key = $this->key($key);
         return $this->_hdl->lPush($key, $val);
     }
 
     function rpush(string $key, $val)
     {
-        $key = $this->key($key);
         return $this->_hdl->rPush($key, $val);
     }
 
     function append(string $key, $val)
     {
-        $key = $this->key($key);
         return $this->_hdl->append($key, $val);
     }
 
     function bitAt(string $key, int $offset): int
     {
-        $key = $this->key($key);
         return $this->_hdl->getBit($key, $offset);
     }
 
     function setbit(string $key, int $offset, $val): int
     {
-        $key = $this->key($key);
         return $this->_hdl->setBit($key, $offset, $val);
     }
 
     function mapSet(string $map, string $key, $value)
     {
-        $map = $this->key($map);
         return $this->_hdl->hSet($map, $key, $value);
     }
 
     function mapGet(string $map, string $key)
     {
-        $map = $this->key($map);
         return $this->_hdl->hGet($map, $key);
     }
 
     function mapSize(string $map): int
     {
-        $map = $this->key($map);
         return $this->_hdl->hLen($map);
     }
 
     function mapDelete(string $map, string $key)
     {
-        $map = $this->key($map);
         return $this->_hdl->hDel($map, $key);
     }
 
     function mapKeys(string $map)
     {
-        $map = $this->key($map);
         return $this->_hdl->hKeys($map);
     }
 
     function mapValues(string $map)
     {
-        $map = $this->key($map);
         return $this->_hdl->hVals($map);
     }
 
     function mapContains(string $map, string $key): bool
     {
-        $map = $this->key($map);
         return $this->_hdl->hExists($map, $key);
     }
 
     function orderSet(string $order, string $val, $score)
     {
-        $order = $this->key($order);
         return $this->_hdl->zAdd($order, $score, $val);
     }
 
     function orderRange(string $order, $start, $end, $scores = null)
     {
-        $order = $this->key($order);
         return $this->_hdl->zRange($order, $start, $end, $scores);
     }
 
     function orderDelete(string $order, string $val)
     {
-        $order = $this->key($order);
         return $this->_hdl->zRem($order, $val);
     }
 
     function orderSize(string $order, string $start, string $end)
     {
-        $order = $this->key($order);
         return $this->_hdl->zCount($order, $start, $end);
     }
 
     function orderGetScore(string $order, string $val)
     {
-        $order = $this->key($order);
         return $this->_hdl->zScore($order, $val);
     }
 
     function orderGetIndex(string $order, string $val)
     {
-        $order = $this->key($order);
         return $this->_hdl->zRank($order, $val);
     }
 
     function orderIncrBy(string $order, string $val, $delta)
     {
-        $order = $this->key($order);
         return $this->_hdl->zIncrBy($order, $delta, $val);
     }
 
     //获取键
     function keys(string $key)
     {
-        $key = $this->key($key);
         $v = $this->_hdl->keys($key);
         if ($v === false)
             return null;
         return $v;
     }
-
-
 }
 
 global $POOLS;
